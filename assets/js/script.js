45352faa2129828c63e892cc2439a33d4f466f0a
arokokko <56476810+arokokko@users.noreply.github.com>
2021-04-14 08:55:38 +0700

    Remove map from footer, add navigation and second address to footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
 
+
 	// tabs
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 		$(this)
@@ -25,87 +26,52 @@ $(document).ready(function(){
 
 	// Modal windows
 
-	// calculate width of scroll line 
-	// function calcScroll() {                               
-	// 	let div = document.createElement('div');
-
-	// 	div.style.width = '50px';
-	// 	div.style.height = '50px';
-	// 	div.style.overflowY = 'scroll';
-	// 	div.style.visibility = 'hidden';
-
-	// 	document.body.appendChild(div);
-	// 	let scrollWidht = div.offsetWidth - div.clientWidth;
-
-	// 	return scrollWidht;
-	// }
-	// scroll line width
-
-	// const scroll = calcScroll();   
-	            
-
-	// const scroll = calcScroll();              
-
-
 	$('[data-modal=consultation]').on('click', function(){
-		$('.overlay, #consultation').fadeIn('slow'); 
-		// $('body').css('overflow', 'hidden');
-		// $('body').css('margin-right', `${scroll}px`);
-		
+		$('.overlay, #consultation').fadeIn('slow');
 	});
 
 	$('[data-modal=question]').on('click', function(){
 		$('.overlay, #question').fadeIn('slow');
-		// $('body').css('overflow', 'hidden');
-		// $('body').css('margin-right', `${scroll}px`);
-		
 	});
-	
+
 
 	$('.card_btn').each(function(i) {
 
         $(this).on('click', function() {
             let descr = $('.card__subtitle').eq(i).text();
             $('#order .modal__descr').text(descr);
-			$('.overlay, #order').fadeIn('slow'); 
-			// $('body').css('overflow', 'hidden');
-			// $('body').css('margin-right', `${scroll}px`);
+			$('.overlay, #order').fadeIn('slow');
         });
 
   	});
 
 	$('.modal__close').on('click', function() {
-		$('.overlay, #consultation, #question, #order, #thanks').fadeOut(); 
-		// $('body').css('overflow', 'visible');
-		// $('body').css('margin-right', `0px`);
+		$('.overlay, #consultation, #question, #order, #thanks').fadeOut();
 	});
 
 	// закрытие модального окна щелчком по пустому полю
 	$(window).on('click', function(e) {
         if (e.target.classList.contains('overlay')) {
-            $('.overlay, #consultation, #question, #thanks, #order').fadeOut(); 
-			// $('body').css('overflow', 'visible');
-			// $('body').css('margin-right', `0px`);
-			
+            $('.overlay, #consultation, #question, #thanks, #order').fadeOut();
         }
 	});
 	
-
 	// отправка писем
-	$('form').submit(function(e) {               /* обращаемся ко всем form */
-		e.preventDefault();                      /* убираем стандартные действия браузера - перезагрузку */
-		$.ajax({                                   /* обращаемся к ajax в jqery */
-			type: 'POST',                         /* говорим что будем отправлять данные на сервер */
-			url: '/wp-content/themes/leadership/assets/mailer/smart.php',               /*  говорим куда будем отправлять */
-			data: $(this).serialize()             /* указываем какие данные отправляем и обрабатываем их для сервера */
+	$('form').submit(function(e) {               
+		e.preventDefault();                      
+		$.ajax({                                   
+			type: 'POST',                         
+			url: '/wp-content/themes/leadership/assets/mailer/smart.php',              
+			data: $(this).serialize()             
 		}).done(function() {
-			$(this).find("input").val("");     /* value всех input устанавливаем в пустую строку */
+			$(this).find("input").val("");     
 			$('#consultation, #question, #order').fadeOut();
 			$('.overlay, #thanks').fadeIn('slow');
-			$('form').trigger('reset');        /* перезагружаем все form */
+			$('form').trigger('reset');        
 		});
 		return false;
 	});
+	
 
 	//появление и исчезание иконки для скрола страницы вверх
 	$(window).scroll(function() {
@@ -130,9 +96,19 @@ $(document).ready(function(){
 		$('html,body').animate({ scrollTop: $(__href).offset().top }, 1000);
 	});
 
-	setTimeout(function(){
-		document.getElementById('map').src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2175.0890137565843!2d24.13462201519957!3d56.964407705363364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfb5269ce9db%3A0x9285344dd284540b!2zQ8STc3UgaWVsYSA1YiwgVmlkemVtZXMgcHJpZWvFoXBpbHPEk3RhLCBSxKtnYSwgTFYtMTAxMiwg0JvQsNGC0LLQuNGP!5e0!3m2!1sru!2s!4v1590760020557!5m2!1sru!2s';
-	  },3000);
+
+	// setTimeout(getMap, 3000);
 	
+	// function getMap() {
+	// 	document.getElementById('map').src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7798.40626024172!2d109.18995827436449!3d12.234521203319698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317067abd48056bb%3A0xcbba08a13cd1b44b!2sLeadership%20TSC!5e0!3m2!1sru!2s!4v1617514419581!5m2!1sru!2s" ;
+	// }
+	
+
+	
+
 });
+
+
+
+
 
--- footer.php
+++ footer.php
@@ -1,12 +1,76 @@
 <footer class="footer">
-        <div class="footer__map"><iframe id="map" src="" width="100%" height="630" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>
-        <div class="footer__contacts">
-            <address class="footer__addr"><?php the_field('address', 2); ?></address>
-            <div class="footer__mail"><a href="mailto:<?php the_field('email', 2); ?>"><?php the_field('email', 2); ?></a></div>
-            <div class="footer__phone"><a href="tel:<?php the_field('phone_link', 2); ?>"><?php the_field('phone_displayed', 2); ?></a></div> 
-            <div class="footer__social">
-                <a href="<?php the_field('facebook', 2); ?>" class="footer__social_item" target='_blank' title='Facebook'><span class="icon-facebook"></span></a>
+        <!-- <div class="footer__map">
+            <iframe id='map' src="" width="100%" height="630" frameborder="0" style="border:0;" allowfullscreen="">
+            </iframe>
+        </div> -->
+        <div class="footer__top">
+            <div class="footer__contacts">
+                <div class="footer__contacts-title">Contact information</div>
+                <div class="footer__contacts-wrapper">
+                    <div class="footer__contact">
+                        <div class="footer__addr"><?php the_field('address', 2); ?></div>
+                        <div class="footer__map">show on the map</div>
+                        <div class="footer__mail">
+                            <span>Email: </span><a href="mailto:<?php the_field('email', 2); ?>"><?php the_field('email', 2); ?></a>
+                        </div>
+                        <div class="footer__phone">
+                            <span>Phone: </span><a href="tel:<?php the_field('phone_link', 2); ?>"><?php the_field('phone_displayed', 2); ?></a>
+                        </div> 
+                        <div class="footer__social">
+                            <a href="<?php the_field('facebook', 2); ?>" class="footer__social_item" target='_blank' title='Facebook'><span class="icon-facebook"></span></a>
+                            <a href="<?php the_field('facebook', 2); ?>" class="footer__social_item" target='_blank' title='Facebook'><span class="icon-facebook"></span></a>
+                            <a href="<?php the_field('facebook', 2); ?>" class="footer__social_item" target='_blank' title='Facebook'><span class="icon-facebook"></span></a>
+                        </div>
+                    </div>
+                    <div class="footer__contact">
+                        <address class="footer__addr"><?php the_field('address', 2); ?></address>
+                        <div class="footer__map">show on the map</div>
+                        <div class="footer__mail">
+                            <span>Email: </span><a href="mailto:<?php the_field('email', 2); ?>"><?php the_field('email', 2); ?></a>
+                        </div>
+                        <!-- <div class="footer__phone">
+                            <a href="tel:<?php the_field('phone_link', 2); ?>"><?php the_field('phone_displayed', 2); ?></a>
+                        </div> 
+                        <div class="footer__social">
+                            <a href="<?php the_field('facebook', 2); ?>" class="footer__social_item" target='_blank' title='Facebook'><span class="icon-facebook"></span></a>
+                        </div> -->
+                    </div>
+                </div>
             </div>
+            <div class="footer__navigation">
+                <div class="footer__navigation-title">Navigation</div>
+                <nav class="footer__navigation-wrapper">
+                    <ul class="nav__list">
+                        <ol class="nav__item">
+                            <a href="https://leadershipnt.com/">Home</a>
+                        </ol>
+                        <ol class="nav__item">
+                            <a href="https://leadershipnt.com/articles/">Articles</a>
+                            <ul class="nav__sublist">
+                                <ol class="nav__subitem">
+                                    <a href="https://leadershipnt.com/legal-advice-info-in-vietnam/">Legal advice</a>
+                                </ol>
+                                <ol class="nav__subitem">
+                                    <a href="https://leadershipnt.com/accounting-info-in-vietnam/">Accounting</a>
+                                </ol>
+                                <ol class="nav__subitem">
+                                    <a href="https://leadershipnt.com/real-estate-info-in-vietnam/">Real estate</a>
+                                </ol>
+                            </ul>
+                        </ol>
+                        <ol class="nav__item">
+                            <a href="https://leadershipnt.com/about-company/">About us</a>
+                        </ol>
+                        <ol class="nav__item">
+                            <a href="https://leadershipnt.com/our-contacts/">Contacts</a>
+                        </ol>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="footer__copyright">2020 © LEADERSHIP TSC. All rights reserved</div>
+            <div class="footer__privacy"><a href="https://leadershipnt.com/privacy-policy/">Privacy Policy</a></div>   
         </div>
     </footer>
 
@@ -17,7 +81,7 @@
             <div class="modal__descr">and we will reply you nearest time</div>
             <form method="POST" action="#" class="form__feed modal_form">
                 <input name="name" required placeholder="Your name" type="text">
-                <input name="phone" required placeholder="Your phone number" type="text">
+                <input name="phone" required placeholder="Your phone number" type="number">
                 <input type="email" name="email" required placeholder="Your E-mail">
                 <textarea name="text" placeholder="Your question"></textarea>
                 <button class="button consultation_btn modal_btn">Ask</button>
@@ -30,7 +94,7 @@
             <div class="modal__descr">and we will reply you nearest time</div>
             <form method="POST" action="#" class="form__feed modal_form">
                 <input name="name" required placeholder="Your name" type="text">
-                <input name="phone" required placeholder="Your phone number" type="text">
+                <input name="phone" required placeholder="Your phone number" type="number">
                 <input type="email" name="email" required placeholder="Your E-mail">
                 <textarea name="text" placeholder="Theme of consultation"></textarea>
                 <button class="button consultation_btn modal_btn">Book consultation</button>
@@ -40,10 +104,10 @@
         <div class="modal" id="order">
             <div class="modal__close">&times;</div>
             <h2 class="modal__subtitle">Your order:</h2>
-            <div class="modal__descr">Type of service</div>
+            <div class="modal__descr">Пульсометер FT-1</div>
             <form method="POST" action="#" class="form__feed modal_form">
                 <input name="name" required placeholder="Your name" type="text">
-                <input name="phone" required placeholder="Your phone number" type="text">
+                <input name="phone" required placeholder="Your phone number" type="number">
                 <input type="email" required name="email" placeholder="Your E-mail">
                 <textarea name="text" placeholder="Additional questions"></textarea>
                 <button class="button consultation_btn modal_btn">Book</button>
@@ -64,6 +128,7 @@
         wp_footer();
     ?>
     
+    <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(75462550, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/75462550" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 
 </body>
 </html>
