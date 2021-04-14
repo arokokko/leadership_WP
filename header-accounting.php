19575b757396145e76d16758b4724301190454ca
arokokko <56476810+arokokko@users.noreply.github.com>
2021-04-14 09:00:21 +0700

    Add navbar to header

--- header-accounting.php
+++ header-accounting.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo('name'); echo ' | '; bloginfo('description'); ?></title>
+    <title><?php the_title(); ?></title>
     <link rel="shortcut icon" href="<?php echo bloginfo('template_url');?>/assets/icons/favicon.ico">
     <?php 
         wp_head();
     ?>
 </head>
 <body>
-    <section id="up" class="promo" style="background-image:url('<?php echo bloginfo('template_url');?>/assets/images/vietnam.png')">
+    <section id="up" class="promo" style="background-image:url('<?php echo bloginfo('template_url');?>/assets/images/vietnam.jpg')">
         <div class="container">
             <header class="header">
                 <div class="header__logotype">
@@ -19,6 +19,17 @@
                         <?php the_custom_logo(); ?>
                     </div>
                 </div>
+                <nav class="promo__icons promo__icons_article">
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/articles/'>
+                        Articles
+                    </a>
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/about-company/'>
+                        About us
+                    </a>
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/our-contacts/'>
+                        Contacts
+                    </a>
+                </nav>
                 <div class="header__contact">
                     <button data-modal="question" class="button header_btn">Ask your question</button>
                 </div>
--- header-article.php
+++ header-article.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php bloginfo('name'); echo ' | '; bloginfo('description'); ?></title>
+    <title><?php the_title(); ?></title> 
     <link rel="shortcut icon" href="<?php echo bloginfo('template_url');?>/assets/icons/favicon.ico">
     <?php 
         wp_head();
     ?>
 </head>
 <body>
-    <section id="up" class="promo_article" style="background-image:url('<?php echo bloginfo('template_url');?>/assets/images/vietnam.png')">
+    <section id="up" class="promo_article" style="background-image:url('<?php echo bloginfo('template_url');?>/assets/images/vietnam1.jpg')">
         <div class="container">
             <header class="header">
                 <div class="header__logotype">
@@ -19,17 +19,17 @@
                         <?php the_custom_logo(); ?>
                     </div>
                 </div>
-                <div class="promo__icons promo__icons_article">
-                    <a class="promo__item promo__item_article" href='http://localhost/leadership/legal-advice-info/'>
-                        Legal advice
+                <nav class="promo__icons promo__icons_article">
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/articles/'>
+                        Articles
                     </a>
-                    <a class="promo__item promo__item_article" href='http://localhost/leadership/real-estate-info/'>
-                        Real estate
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/about-company/'>
+                        About us
                     </a>
-                    <a class="promo__item promo__item_article" href='http://localhost/leadership/accounting-info/'>
-                        Accounting
+                    <a class="promo__item promo__item_article" href='https://leadershipnt.com/our-contacts/'>
+                        Contacts
                     </a>
-                </div>
+                </nav>
                 <div class="header__contact">
                     <button data-modal="question" class="button header_btn">Ask your question</button>
                 </div>
